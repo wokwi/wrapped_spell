6912e611865767f1fad41109754d56c4c2c9e0eb
Uri Shaked <uri@urishaked.com>
2021-12-11 15:33:49 +0200

    chore: reduce die size to 430x430
    it's the minimal size that doesn't fail the hardening flow

--- config.tcl
+++ config.tcl
@@ -20,8 +20,8 @@ set ::env(PL_TARGET_DENSITY) 0.4
 # don't put clock buffers on the outputs, need tristates to be the final cells
 set ::env(PL_RESIZER_BUFFER_OUTPUT_PORTS) 0
 
-# set absolute size of the die to 300 x 300 um
-set ::env(DIE_AREA) "0 0 500 500"
+# set absolute size of the die to 430 x 430 um
+set ::env(DIE_AREA) "0 0 430 430"
 set ::env(FP_SIZING) absolute
 
 # define number of IO pads
