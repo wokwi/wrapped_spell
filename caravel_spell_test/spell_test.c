3ef470b51660d0646d083e5d1848b68b1612827b
Uri Shaked <uri@urishaked.com>
2021-12-17 22:51:33 +0200

    test: add openram to the integration test

--- caravel_spell_test/spell_test.c
+++ caravel_spell_test/spell_test.c
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: © 2021 Uri Shaked <uri@wokwi.com>
 // SPDX-License-Identifier: MIT
 
+
+#include "verilog/dv/caravel/defs.h"
+
 #define PROJECT_ID              1
 
 #define reg_spell_pc            (*(volatile uint32_t*)0x30000000)
@@ -11,10 +14,19 @@
 #define reg_spell_stack_top     (*(volatile uint32_t*)0x30000014)
 #define reg_spell_stack_push    (*(volatile uint32_t*)0x30000018)
 
-#define TEST_RESULT_PASS        0x1
-#define TEST_RESULT_FAIL        0xf
+#define CTRL_RUN (1 << 0)
+#define CTRL_STEP (1 << 1)
+#define CTRL_SRAM_ENABLE (1 << 2)
 
-#include "verilog/dv/caravel/defs.h"
+#define SRAM_WRITE_PORT         31
+#define SRAM_BASE_ADDR          0x30FFFC00
+#define OPENRAM(addr)           (*(uint32_t*)(SRAM_BASE_ADDR + (addr & 0x3fc)))
+
+#define PACK_SPELL(a, b, c, d) ((a) | (b << 8) | (c << 16) | (d << 24))
+
+#define TEST_RESULT_PASS        0x1
+#define TEST_RESULT_FAIL_SRAM   0xe
+#define TEST_RESULT_FAIL_DFF    0xf
 
 void write_progmem(uint8_t addr, uint8_t opcode) {
     reg_spell_stack_push = opcode;
@@ -48,16 +60,44 @@ void main() {
     write_progmem(3, 'z');
 
     // Start SPELL
-    reg_spell_ctrl = 1;
+    reg_spell_ctrl = CTRL_RUN;
 
     // Wait for the SPELL to finish
-    while (reg_spell_ctrl & 0x1);
+    while (reg_spell_ctrl & CTRL_RUN);
 
     // At this point, we should have 55 at the top of the stack.
-    if (reg_spell_stack_top == 55) {
-        reg_mprj_datal = TEST_RESULT_PASS << 28;
-    } else {
-        reg_mprj_datal = TEST_RESULT_FAIL << 28;
+    if (reg_spell_stack_top != 55) {
+        reg_mprj_datal = TEST_RESULT_FAIL_DFF << 28;
+        return;
     }
+
+    // Reset SPELL
+    reg_la1_data |= 1;
+    reg_la1_data &= ~1;
+
+    // Write a program to the OpenSRAM
+    OPENRAM(0) = PACK_SPELL(
+        90,
+        14,
+        '-',
+        'z'
+    );
+
+    // Note: uncommenting the following line causes test to fail
+    // reg_la0_data = (1 << SRAM_WRITE_PORT) | (1 << PROJECT_ID);
+
+    // Start SPELL
+    reg_spell_ctrl = CTRL_RUN | CTRL_SRAM_ENABLE;
+
+    // Wait for the SPELL to finish
+    while (reg_spell_ctrl & CTRL_RUN);
+
+    // At this point, we should have (90-14) at the top of the stack.
+    if (reg_spell_stack_top != 90-14) {
+        reg_mprj_datal = TEST_RESULT_FAIL_SRAM << 28;
+        return;
+    }
+    
+    reg_mprj_datal = TEST_RESULT_PASS << 28;
 }
 
--- caravel_spell_test/test_spell.py
+++ caravel_spell_test/test_spell.py
@@ -5,24 +5,24 @@ from cocotb.triggers import RisingEdge, ClockCycles, with_timeout
 TEST_RESULT_PASS = 0x1 # Should be in sync with spell_test.c 
 
 @cocotb.test()
-async def test_start(dut):
+async def test_integration(dut):
     clock = Clock(dut.clk, 25, units="ns") # 40M
     cocotb.fork(clock.start())
     
     dut.RSTB <= 0
-    dut.power1 <= 0;
-    dut.power2 <= 0;
-    dut.power3 <= 0;
-    dut.power4 <= 0;
+    dut.power1 <= 0
+    dut.power2 <= 0
+    dut.power3 <= 0
+    dut.power4 <= 0
 
     await ClockCycles(dut.clk, 8)
-    dut.power1 <= 1;
+    dut.power1 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power2 <= 1;
+    dut.power2 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power3 <= 1;
+    dut.power3 <= 1
     await ClockCycles(dut.clk, 8)
-    dut.power4 <= 1;
+    dut.power4 <= 1
 
     await ClockCycles(dut.clk, 80)
     dut.RSTB <= 1
@@ -31,7 +31,7 @@ async def test_start(dut):
     await with_timeout(RisingEdge(dut.uut.mprj.wrapped_spell_1.active), 180, 'us')
 
     # Wait
-    await ClockCycles(dut.clk, 8000)
+    await ClockCycles(dut.clk, 9200)
 
     # Check if C program ran succesfully
     assert(((int(dut.uut.soc.mgmt_out_predata) >> 28) & 0xf) == TEST_RESULT_PASS)
